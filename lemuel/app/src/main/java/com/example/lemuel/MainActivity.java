fdf19161614a3b0e19c4ca9deb0a4ca3b653cf60
r0ltsh <lemuel.cs17@bmsce.ac.in>
2020-04-20 22:01:55 +0530

    database connected

--- lemuel/app/src/main/java/com/example/lemuel/MainActivity.java
+++ lemuel/app/src/main/java/com/example/lemuel/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.lemuel;
 
+import androidx.annotation.NonNull;
 import  androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+import java.util.Map;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_NAME = "com.example.lemuel.NAME";
     public static final String EXTRA_USN = "com.example.lemuel.USN";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +40,10 @@ public class MainActivity extends AppCompatActivity {
         EditText editUSN = (EditText) findViewById(R.id.editText7);
         String Name = editName.getText().toString();
         String USN = editUSN.getText().toString();
+
         intent.putExtra(EXTRA_NAME, Name);
         intent.putExtra(EXTRA_USN, USN);
         startActivity(intent);// Do something in response to button
     }
+
 }
